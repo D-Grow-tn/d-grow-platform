68a74f8b549a906d4f3d669c52a69e298c5613c0
khalil kraiem <khalilmedkraiem@gmail>
2023-05-15 16:28:31 +0100

    hover underline component

---
+++ client/src/components/TextHoverUnderline.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function TextHoverUnderline({
+  width,
+  height,
+  color,
+  bgColor,
+  duration,
+  content,
+  type,
+}) {
+  let style = {
+    width: width ? width : "",
+    height: height ? height : "",
+    color: color ? color : "",
+    backgroundColor: bgColor ? bgColor : "",
+  };
+  return (
+    <div
+      className="hover-underline-transition pointer d-flex justify-content-center align-items-center"
+      style={{ width: width, position: "relative" }}
+    >
+      <h6 className="m-2">{content}</h6>
+      <div
+        className="hover-underline-transition-line"
+        style={
+          duration
+            ? { transition: `width ${duration}ms ${type}`, ...style }
+            : style
+        }
+      />
+    </div>
+  );
+}
+
+export default TextHoverUnderline;
