80d69a76d227a4db6ea3bcf7823f48c8e1b87274
ines-sudo <inesdebbichi1995@gmail.com>
2023-06-02 15:28:13 +0200

    aboutUs graphic integration

--- client/src/assets/img/images.js
+++ client/src/assets/img/images.js
@@ -1,2 +1,7 @@
 export const background2=require('./background2.png')
-export default {background2}
+export const jalyss1=require('./jalyss1.png')
+export const jalyss2=require('./jalyss2.png')
+export const jalyss3=require('./jalyss3.png')
+export const jalyss4=require('./jalyss4.png')
+export const jalyss5=require('./jalyss5.png')
+export default {background2,jalyss1,jalyss2,jalyss3,jalyss4,jalyss5}
