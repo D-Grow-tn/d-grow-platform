59ceee2a73a2fbfa8498082bb2407952f470c8e4
Ahmed henchiri <ahmedhenchiri00@gmail.com>
2023-06-06 13:46:02 +0100

    add login and ResetPasssword form using bootstrap

--- back-office/src/componets/CastomForm.js
+++ back-office/src/componets/CastomForm.js
@@ -13,86 +13,94 @@ import {
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-
 import "../css/LoginAdmin.css";
 function CastomForm({ Name, ButtonName }) {
-
   return (
-    <MDBContainer fluid className="my-5 ">
-      <MDBRow className="g-0 align-items-center">
-        <MDBCol col="6">
-          <MDBCard
-            className="my-5 cascading-right"
-            style={{
-              background: "hsla(0, 0%, 100%, 0.55)",
-              backdropFilter: "blur(30px)",
-            }}
+    <div className="bg-light">
+      <MDBContainer fluid className="my-5  ">
+        <MDBRow className="g-0 align-items-center">
+          <MDBCol
+            col="6"
+            className="d-flex justify-content-center align-items-center"
           >
-            <MDBCardBody className="p-5 shadow-5 text-center ">
-              <h2 className="fw-bold mb-5">{Name} </h2>
-              {Name === "Forgot Password" && (
-                <>
-                  <MDBInput
-                    wrapperClass="mb-4"
-                    label="Email"
-                    id="form3"
-                    type="email"
-                  />
-                  <div className="d-flex justify-content-end">
-                    {" "}
-                    <Link to="/loginAdmin">Back to Login </Link>
-                  </div>
-                </>
-              )}
-              {Name === "Login" && (
-                <>
-                  <MDBInput
-                    wrapperClass="mb-4"
-                    label="Email"
-                    id="form3"
-                    type="email"
-                  />
-                  <MDBInput
-                    wrapperClass="mb-4"
-                    label="Password"
-                    id="form4"
-                    type="password"
-                  />
+            <MDBCard
+              className="my-5 cascading-right"
+              style={{
+                background: "hsla(0, 0%, 100%, 0.55)",
+                backdropFilter: "blur(30px)",
+              }}
+            >
+              <MDBCardBody className="p-5 shadow-5 text-center w-100">
+                <h2 className="fw-bold mb-5">{Name} </h2>
+                {Name === "Forgot Password" && (
+                  <>
+                    <div className="input d-flex align-items-start">
+                      <label>
+                        <i className="fa-solid fa-user p-1 mt-2"></i>
+                      </label>
+                      <input
+                        placeholder="Email"
+                        className="mb-4 border border-secondary rounded form-control form-control-lg custom-height"
+                        type="email"
+                      />
+                    </div>
 
-                  <div className="d-flex  mb-4">
-                    <MDBCheckbox
-                      name="flexCheck"
-                      value=""
-                      id="flexCheckDefault"
-                      label="Subscribe to our newsletter"
-                    />
-                  </div>
-                  <div className="d-flex justify-content-end">
-                    {" "}
-                    <Link to="/resetPassword">Reset Password?</Link>
-                  </div>
-                </>
-              )}
-              <div className="d-flex justify-content-center ">
-                <Button className="w-100 mb-5 d-flex justify-content-center ">
-                  {" "}
-                  {ButtonName}{" "}
-                </Button>
-              </div>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
+                    <div className="d-flex justify-content-end my-3">
+                      {" "}
+                      <Link to="/loginAdmin">Back to Login </Link>
+                    </div>
+                  </>
+                )}
+                {Name === "Login" && (
+                  <>
+                    <div className="input d-flex align-items-start">
+                      <label>
+                        <i className="fa-solid fa-user p-1 mt-2"></i>
+                      </label>
+                      <input
+                        placeholder="Email"
+                        className="mb-4 border border-secondary rounded form-control form-control-lg custom-height"
+                        type="email"
+                      />
+                    </div>
 
-        <MDBCol col="6">
-          <img
-            src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg"
-            class="w-100 rounded-4 shadow-4"
-            alt=""
-            fluid
-          />
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+                    <div className="input d-flex align-items-start">
+                      <label>
+                        <i className="fa-solid fa-key p-1 mt-3"></i>
+                      </label>
+                      <input
+                        placeholder="Password"
+                        className="mb-4 border border-secondary rounded form-control form-control-lg custom-height"
+                        type="Password"
+                      />
+                    </div>
+
+                    <div className="d-flex  mb-4">
+                      <MDBCheckbox
+                        name="flexCheck"
+                        value=""
+                        id="flexCheckDefault"
+                        label="Subscribe to our newsletter"
+                      />
+                    </div>
+                    <div className="d-flex justify-content-end my-3 ">
+                      {" "}
+                      <Link to="/resetPassword">Reset Password?</Link>
+                    </div>
+                  </>
+                )}
+                <div className="d-flex justify-content-center ">
+                  <Button className="w-100 mb-5 d-flex justify-content-center btn custom-button ">
+                    {" "}
+                    {ButtonName}{" "}
+                  </Button>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </div>
   );
 }
 
