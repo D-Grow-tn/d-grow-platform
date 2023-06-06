a681ffc88b82bf80dc3d2d400beab2bf99906688
raniaelouni <raniaelouni421@gmail.com>
2023-06-06 14:49:25 +0100

    projectDetailTest

---
+++ client/src/constants/projectDetailData.js
@@ -0,0 +1,38 @@
+const navData = [
+    {
+      nameEn: "Objectives",
+      nameAr: "",
+      nameFr: "",
+      path: "",
+      image:
+        "https://tourduvalat.org/wp-content/uploads/2017/11/bullseye2-512.png",
+      colorTab: "blue",
+    },
+    {
+      nameEn: "Gantt Diagram",
+      nameAr: "",
+      nameFr: "Diagramme de Gantt",
+      path: "/gantt",
+      image: "https://cdn-icons-png.flaticon.com/512/5639/5639804.png",
+      colorTab: "green",
+    },
+    {
+      nameEn: "Agent In Charge",
+      nameAr: "",
+      nameFr: "Agents responsables",
+      path: "/team-section",
+      image: "https://cdn-icons-png.flaticon.com/128/4059/4059783.png",
+      colorTab: "blue",
+    },
+    {
+      nameEn: "Intercations",
+      nameAr: "",
+      nameFr: "Interactions",
+      path: "/interaction",
+      image:
+        "https://www.iconarchive.com/download/i86695/johanchalibert/mac-osx-yosemite/messages.1024.png",
+      colorTab: "purple",
+    },
+  ];
+
+  export default navData
